53d81ece115adb78c86412453c2809883c3c17ea
DPT1004 <phuthanhdo104@gmail.com>
2023-04-17 21:52:11 +0700

    Sign In with facebook

--- src/Screen/SignIn/SignIn.js
+++ src/Screen/SignIn/SignIn.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Button, Text, Touchable, TouchableOpacity, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 import { screenName } from '../../navigator/screens-name'
 import { useNavigation } from "@react-navigation/native"
+import { LoginButton, AccessToken } from 'react-native-fbsdk-next';
 
 
 const SignIn = () => {
 
     const navigation = useNavigation()
+
     return (
         <View>
             <Text>This is Sign In</Text>
@@ -15,6 +17,24 @@ const SignIn = () => {
                 color={"red"}
                 onPress={() => navigation.navigate(screenName.SignUp)}
             />
+            <LoginButton
+                onLoginFinished={
+                    (error, result) => {
+                        if (error) {
+                            console.log("login has error: " + result.error);
+                        } else if (result.isCancelled) {
+                            console.log("login is cancelled.");
+                        } else {
+                            AccessToken.getCurrentAccessToken().then(
+                                (data) => {
+                                    console.log(data.accessToken.toString())
+                                }
+                            )
+                        }
+                    }
+                }
+                onLogoutFinished={() => console.log("logout.")} />
+
         </View>
     );
 };
